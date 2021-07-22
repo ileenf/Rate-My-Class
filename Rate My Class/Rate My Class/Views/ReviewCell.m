dac8d20c776306c07259703a8ae44118a724751d
ileenf <ifan2011@gmail.com>
2021-07-22 09:55:24 -0700

    Updated likes logic

--- Rate My Class/Rate My Class/Views/ReviewCell.m
+++ Rate My Class/Rate My Class/Views/ReviewCell.m
@@ -12,19 +12,22 @@
 @implementation ReviewCell
 
 - (IBAction)didTapLike:(id)sender {
+    self.tempUsersLiked = self.review.usersLiked;
     if ([self.review.usersLiked containsObject:[PFUser currentUser].username]) {
         int value = [self.review.likeCount intValue];
         self.review.likeCount = [NSNumber numberWithInt:value - 1];
         
         [self.likeIcon setSelected: NO];
-        [self.review.usersLiked removeObject:[PFUser currentUser].username];
+        [self.tempUsersLiked removeObject:[PFUser currentUser].username];
     } else {
         int value = [self.review.likeCount intValue];
         self.review.likeCount = [NSNumber numberWithInt:value + 1];
         
         [self.likeIcon setSelected: YES];
-        [self.review.usersLiked addObject:[PFUser currentUser].username];
+        [self.tempUsersLiked addObject:[PFUser currentUser].username];
     }
+    NSLog(@"%@", self.review.usersLiked);
+    self.review.usersLiked = self.tempUsersLiked;
     self.likeCountLabel.text = [NSString stringWithFormat:@"%@", self.review.likeCount];
     [self.review saveInBackground];
 }
